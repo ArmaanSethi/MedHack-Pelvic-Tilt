94f568810ba73d6e886dc36b5a36d38de565c0d8
Armaan Sethi <armaan.sethi@gmail.com>
2017-09-09 23:50:55 -0400

    update stuff

--- server.py
+++ server.py
@@ -7,21 +7,24 @@ import label_image
 def script_caller(image):
         graph = "retrained_graph.pb" # on site
         image = "images/post/20.jpeg" # user input
+
         textFile = "retrained_labels.txt" #on the site
 
         return label_image.function(graph, image, textFile)
 
 @app.route("/", methods=['GET', 'POST'])
 def root():
-    image = "images/ant/1.jpeg"
+    return render_template('index.html')
+
+@app.route('/results')
+def do_things(image = None):
     (labels, confidences) = script_caller(image)
     the_label = "None"
     for i in range(len(labels)):
         if(confidences[i]) > 0.5:
             the_label = labels[i]
             the_confidence = confidences[i]
-
-    return render_template('index.html', label = the_label, confidence = the_confidence)
+    return render_template('results.html', label = the_label, confidence = the_confidence)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
