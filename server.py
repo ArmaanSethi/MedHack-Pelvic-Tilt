91907734150208e9216e2f99832753bdae047644
Armaan Sethi <armaan.sethi@gmail.com>
2017-09-27 18:35:57 -0400

    fix cache struggles

--- server.py
+++ server.py
@@ -11,6 +11,19 @@ def script_caller(image):
         textFile = "retrained_labels.txt" #on the site
         return label_image.function(graph, image, textFile)
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route("/", methods=['GET', 'POST'])
 def root():
     return render_template('index.html')
